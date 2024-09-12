841c31eb3b3fc2d4b5e587ff43f22031cc65cd82
Jenny Penny <jpenny@embracepetinsurance.com>
2024-09-12 13:49:49 -0400

    finished count words assignment

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    count = count_words(text)
+    print(count)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(text):
+    num_words = 0
+    words = text.split()
+    
+    for word in words:
+        num_words += 1
+    return num_words
+
+main()

78871bea38a967f4d595df410be5a9e2c8f4f248
Jenny Penny <jpenny@embracepetinsurance.com>
2024-09-12 14:56:29 -0400

    finished char count and modified count_words function

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     count = count_words(text)
-    print(count)
+    character_count = count_characters(text)
+    print(character_count)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def count_words(text):
-    num_words = 0
     words = text.split()
-    
-    for word in words:
-        num_words += 1
-    return num_words
+    return len(words)
+
+def count_characters(text):
+    lower_case = text.lower()
+    char_count = {}
+
+    for char in lower_case:
+        char_count[char] = char_count.get(char, 0) + 1
+    return char_count
 
 main()
+
+

e6183388dd14730216a3365f7b2dae1379fb966e
Jenny Penny <jpenny@embracepetinsurance.com>
2024-09-17 10:29:57 -0400

    final lesson for project

--- main.py
+++ main.py
@@ -3,7 +3,19 @@ def main():
     text = get_book_text(book_path)
     count = count_words(text)
     character_count = count_characters(text)
-    print(character_count)
+    char_list = convert(character_count)
+
+    #sort the list of dictionaries
+    char_list.sort(key=sort_on, reverse=True)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f" {count} words found in the document\n")
+    
+    for char_dict in char_list:
+        if char_dict['char'].isalpha():
+            print(f" The '{char_dict['char']}' character was found {char_dict['num']} times")
+    
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
@@ -20,6 +32,13 @@ def count_characters(text):
     for char in lower_case:
         char_count[char] = char_count.get(char, 0) + 1
     return char_count
+
+def convert(character_count):
+    return [{'char': char, 'num': count} for char, count in character_count.items()]
+
+def sort_on(dict):
+    return dict["num"]
+
 
 main()
 
